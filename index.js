1d1567553c052b45777d23bab8257e312a137051
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-19 17:33:48 -0600

    engine to js and other folders sorted

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
+    .set('view engine', 'js')
     // basic pages
     .get('/', (req, res) => res.render('pages/basic/index'))
     .get('/dashboard', (req, res) => res.render('pages/basic/dashboard'))
     .get('/signup', (req, res) => res.render('pages/basic/signup'))
     // discussion pages
-    /*
-    .get('/discussion', (req, res) => res.render('pages/discussion'))
-    .get('/dashboard', (req, res) => res.render('pages/dashboard'))
-    .get('/dashboard', (req, res) => res.render('pages/dashboard'))
-    .get('/dashboard', (req, res) => res.render('pages/dashboard'))
-    .get('/dashboard', (req, res) => res.render('pages/dashboard'))*/
+    .get('/discussion', (req, res) => res.render('pages/discussion/discussion'))
+    .get('/discussion/content', (req, res) => res.render('pages/discussion/discussion-content'))
+    // game pages
+    .get('/game/canvas', (req, res) => res.render('pages/game/game-canvas'))
+    .get('/game/minesweeper', (req, res) => res.render('pages/game-minesweeper'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
