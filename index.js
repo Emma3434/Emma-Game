dbc1d78341425093f652fd9c5df19145d64e6810
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-20 15:56:33 -0600

    header reform src

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 express()
     .set('view engine', 'ejs')
     .use(express.static(path.join(__dirname, 'public')))
-    .use(express.static(path.join(__dirname, 'public')))
+    .use(express.static(path.join(__dirname, 'src')))
     .set('views', path.join(__dirname, 'views'))
 
     // basic pages
