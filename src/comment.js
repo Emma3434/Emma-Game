2f4cebb378013e5f44bca39988933d07bbff0a8c
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-22 17:13:28 -0600

    get discussion from id

--- src/comment.js
+++ src/comment.js
@@ -3,22 +3,17 @@ function get_messages()
 {
     var myHeaders = new Headers();
     myHeaders.append("Authorization", localStorage.getItem("token"));
-    myHeaders.append("Content-Type", "application/json");
-
-    var raw = JSON.stringify({"username":localStorage.getItem("username"),
-        "topic":localStorage.getItem("topic")});
 
     var requestOptions = {
         method: 'GET',
         headers: myHeaders,
-        body: raw,
         redirect: 'follow'
     };
 
-    fetch("https://emma-game-server.herokuapp.com/comment", requestOptions)
+    fetch("https://emma-game-server.herokuapp.com/discussions/5ebf10fa0948030004a91b50", requestOptions)
         .then(response => response.text())
         .then(result => {
-            console.log(result);
+            console.log(result)
             for (i = 0 ; i < result.discussion.comments.length ; i++){
                 $('<li class="list-group-item align-items-xl-start comment"><img class="profile-chat" src="../image/default.jpg"><div><span class="d-block">'+result.discussion.comments[i].username+'</span><span class="border rounded border-primary shadow-sm d-block message">'+ result.discussion.comments[i].message +'</span><span class="d-block">'+result.discussion.comments[i].time+'</span></div></li>').appendTo($('#discussion'));
             }
