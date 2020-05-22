3dea04c0924bdf128f6bd7127a9dc3e002a7d1a6
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-21 18:58:40 -0600

    save topic added to the discussion's topic onclick

--- src/discussion.js
+++ src/discussion.js
@@ -17,9 +17,13 @@ function get_discussion()
             console.log(result);
             for(i = 0; i < result.discussion.length;i++)
             {
-                $('<li class="list-group-item"><span class="disscussion-title"><a id="discussion-heading" href="https://emma-game.herokuapp.com/discussion/content">#'+result.discussion[i].topic+'</a></span><span id="discussion-admin" class="discussion-admin">@'+result.discussion[i].admin+'</span><span id="discussion-description" class="discussion-description">'+result.discussion[i].description+'</span></li>').appendTo($('#discussions'));
+                $('<li class="list-group-item"><span class="disscussion-title"><a id="discussion-heading" href="https://emma-game.herokuapp.com/discussion/content" onclick="save_topic()">#'+result.discussion[i].topic+'</a></span><span id="discussion-admin" class="discussion-admin">@'+result.discussion[i].admin+'</span><span id="discussion-description" class="discussion-description">'+result.discussion[i].description+'</span></li>').appendTo($('#discussions'));
             }
 
         })
         .catch(error => console.log('error', error));
+}
+
+function save_topic(){
+    localStorage.setItem("topic", document.getElementById("discussion-heading").value);
 }
