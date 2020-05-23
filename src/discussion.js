6f87caa1abb150345214fb89020a03a5ece29aed
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-22 20:09:38 -0600

    discussion id test

--- src/discussion.js
+++ src/discussion.js
@@ -17,14 +17,16 @@ function get_discussion()
             console.log(result);
             for(i = 0; i < result.discussion.length;i++)
             {
-                $('<li class="list-group-item"><span class="disscussion-title"><a id="discussion-heading" href="https://emma-game.herokuapp.com/discussion/content" onclick="save_id()">#'+result.discussion[i].topic+'</a></span><span id="discussion-admin" class="discussion-admin">@'+result.discussion[i].admin+'</span><span id="discussion-description" class="discussion-description">'+result.discussion[i].description+'</span><p id="discussion-id" hidden>'+result.discussion[i].id+'</p></li>').appendTo($('#discussions'));
+                $('<li class="list-group-item"><span class="disscussion-title"><a id="discussion-heading"  onclick="save_id()">#'+result.discussion[i].topic+'</a></span><span id="discussion-admin" class="discussion-admin">@'+result.discussion[i].admin+'</span><span id="discussion-description" class="discussion-description">'+result.discussion[i].description+'</span><p id="discussion-id" hidden>'+result.discussion[i].id+'</p></li>').appendTo($('#discussions'));
             }
 
         })
         .catch(error => console.log('error', error));
 }
 
+//href="https://emma-game.herokuapp.com/discussion/content"
 function save_id(){
     localStorage.setItem("topic", document.getElementById("discussion-id").value);
+    console.log(document.getElementById("discussion-id"));
     console.log(localStorage.getItem("topic"));
 }
