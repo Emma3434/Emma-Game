b61385b1ad9c6532f4b3c8648c6d340bfa8a26ad
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-22 22:05:50 -0600

    Update discussion.js

--- src/discussion.js
+++ src/discussion.js
@@ -17,7 +17,7 @@ function get_discussion()
             console.log(result);
             for(i = 0; i < result.discussion.length;i++)
             {
-                $('<li class="list-group-item"><span class="disscussion-title"><a id="discussion-heading"  onclick="save_id()" href = "https://emma-game.herokuapp.com/discussion/'+result.discussion[i]._id+'">#'+result.discussion[i].topic+'</a></span><span id="discussion-admin" class="discussion-admin">@'+result.discussion[i].admin+'</span><span id="discussion-description" class="discussion-description">'+result.discussion[i].description+'</span><p id="discussion-id'+i+'">'+result.discussion[i]._id+'</p></li>').appendTo($('#discussions'));
+                $('<li class="list-group-item"><span class="disscussion-title"><a id="discussion-heading" href = "https://emma-game.herokuapp.com/discussion/'+result.discussion[i]._id+'">#'+result.discussion[i].topic+'</a></span><span id="discussion-admin" class="discussion-admin">@'+result.discussion[i].admin+'</span><span id="discussion-description" class="discussion-description">'+result.discussion[i].description+'</span><p id="discussion-id'+i+'">'+result.discussion[i]._id+'</p></li>').appendTo($('#discussions'));
             }
 
         })
